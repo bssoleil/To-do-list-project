ea67ccd9c0f35853be5e8eacea239876a57406d3
bssoleil <sadia.bentahar59@gmail.com>
2019-04-07 21:53:03 +0200

    added js code for crossed out item( if purchased) in script.js file

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function newElement(){
     }
 
 }
+
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
